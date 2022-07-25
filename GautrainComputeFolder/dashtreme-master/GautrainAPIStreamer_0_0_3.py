3523d46b64e7bd9e034dc46e73eeb69d7c087cac
Moloko Cordei Mokubedi <molavovic@gmail.com>
2022-07-25 23:26:06 +0200

    runit

--- GautrainComputeFolder/dashtreme-master/GautrainAPIStreamer_0_0_3.py
+++ GautrainComputeFolder/dashtreme-master/GautrainAPIStreamer_0_0_3.py
@@ -84,7 +84,7 @@ while(True):
     #json_object = json.dumps(data_json, indent=4)
 
     #write_geojson(geo_json, filename='API_call_outputs/GeolocateAPICall0.html', indent=4)
-    write_geojson(geo_json, filename='index.geojson', indent=4)
+    write_geojson(geo_json, filename='GautrainComputeFolder/dashtreme-master/index.geojson', indent=4)
 
     print(f'Wrote dataset number {gjnum} to outputs.')
     gjnum = gjnum + 1
